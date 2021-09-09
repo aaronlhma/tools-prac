c5115361ee06a83044580b6ba029b9069429cfd8
aaronlhma <aaronlhma@gmail.com>
2021-09-09 13:07:00 -0700

    working on adding jenkins

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -84,6 +84,14 @@ resource "aws_security_group" "allow_ssh" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        description ="jenkins"
+        from_port =8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress{
         from_port =0
         to_port = 0
